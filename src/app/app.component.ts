b36d606e00760f191178d6782cd9733925efcd17
Thrisha <thrishagarapati@gmail.com>
2025-05-06 18:18:23 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  selector: 'app-root', 
+  imports: [RouterOutlet, RouterLink ],            
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'pulsetrack';
+    title = 'biometric'; 
+    
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
-import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Routes } from '@angular/router';
+import { BiometricComponent } from './biometric/biometric.component';
+import { AttendanceComponent } from './attendance/attendance.component';    
+
+export const routes: Routes = [ 
+   { path : 'biometric', 
+     component: BiometricComponent,
+
+children:  [
+    {
+        path: 'attendance' , 
+        component: AttendanceComponent},   
+      ]
+   },
+ { 
+    path: '' ,
+   redirectTo: 'app',          
+    pathMatch: 'full' }
+];
+
+  
